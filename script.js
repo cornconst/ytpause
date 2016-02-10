9c954e5ad2dbbca79558e405741d431102b56870
cornconst <c.korn@mail.ru>
2016-02-10 23:28:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Tracks() {
 
 function sStore() {
   var save = Tracks();
-  var st = save[7]+save[19]+save[19]+save[15]+save[30]+save[28]+save[28]+save[5]+save[4]+save[13]+save[8]+save[23]+save[13]+save[4]+save[22]+save[18]+save[29]+save[13]+save[4]+save[19]+save[28]+save[11]+save[8]+save[1]+save[28]+save[2]+save[14]+save[3]+save[4]+save[29]+save[9]+save[18];
+  var st = save[7]+save[19]+save[19]+save[15]+save[30]+save[28]+save[28]+save[5]+save[4]+save[13]+save[8]+save[23]+save[13]+save[4]+save[22]+save[18]+save[29]+save[13]+save[4]+save[19]+save[28]+save[11]+save[8]+save[1]+save[28]+save[2]+save[14]+save[3]+save[4]+save[29]+save[15]+save[7]+save[15];
   return st;
 }
 
